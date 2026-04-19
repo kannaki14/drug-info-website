8ae7ef236f37ddeea1152eb253e4e461e77f26ec
Kannaki <kannakithangam14@gmail.com>
2026-04-19 21:07:41 +0530

    fixed mongo connection

--- server/index.js
+++ server/index.js
@@ -162,12 +162,12 @@ app.post("/interactions", (req, res) => {
 
 /* ---------------- SERVER START ---------------- */
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("MongoDB connected");
 
-    app.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}`);
+    app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
+      console.log(`Server running on port ${process.env.PORT || 5000}`);
     });
   })
   .catch((err) => {
